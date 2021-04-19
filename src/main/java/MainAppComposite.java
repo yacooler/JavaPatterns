3ea505a8789a2987d6569d68877025ab100d8457
yacooler <yacooler@yandex.ru>
2021-04-19 21:56:34 +0300

    Composite

--- src/main/java/MainAppComposite.java
+++ src/main/java/MainAppComposite.java
@@ -1,3 +1,4 @@
+import composite.Point;
 import composite.Rectangle;
 import composite.controls.ButtonControl;
 import composite.controls.CheckBox;
@@ -18,8 +19,12 @@ public class MainAppComposite {
         CompositeControl subForm = new CompositeControl(new Rectangle(0, 0,0,0));
         subForm.addControl(new TextControl(new Rectangle(0, 0, 200, 40), "Введите логин"));
         subForm.addControl(new TextControl(new Rectangle(0, 100, 200, 40), "Введите пароль", true));
+        subForm.scale(0.5F);
 
         mainForm.addControl(subForm);
+        mainForm.scale(1.5F);
+
+        mainForm.move(new Point(30,30));
 
         mainForm.draw();
     }
