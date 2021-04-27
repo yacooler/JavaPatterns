ecc749b3cf4c2bbb5cb3a29ae9ee24c7dd587f97
yacooler <yacooler@yandex.ru>
2021-04-27 22:57:07 +0300

    Добавлены IdentityMap для пользователя и продукта

---
+++ src/main/java/identitymap/IdentityMap.java
@@ -0,0 +1,5 @@
+package identitymap;
+
+public interface IdentityMap <T, C>{
+    T getById(C c);
+}
---
+++ src/main/java/identitymap/UserIdentityMap.java
@@ -0,0 +1,32 @@
+package identitymap;
+
+import mapper.BaseMapper;
+import mapper.entity.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserIdentityMap implements IdentityMap<User, Long> {
+
+    private final Map<Long, User> userMap = new HashMap<>();
+    private final BaseMapper<User, Long> baseMapper;
+
+
+    //В спринге была бы инъекция
+    public UserIdentityMap(BaseMapper<User, Long> baseMapper) {
+        this.baseMapper = baseMapper;
+    }
+
+    @Override
+    public User getById(Long aLong) {
+        User user = userMap.get(aLong);
+        if (user == null) {
+            user = baseMapper.getById(aLong);
+            if (user != null) {
+                userMap.put(aLong, user);
+                return user;
+            }
+        }
+        return null;
+    }
+}
